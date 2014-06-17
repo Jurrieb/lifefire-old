d932d3dd1d3103a8f52731d9148a75471478e8fe
Netventief <info@netventief.nl>
2014-06-17 15:30:26 +0200

    Create relations

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,14 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    # Find of create user
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
 
+    # Create relations
+    @user.create_relations if !@user.userPreference && !@user.userDetail
+
     if @user.persisted?
-      @user.create_relations(@user.id)
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
     end
   end
 end
