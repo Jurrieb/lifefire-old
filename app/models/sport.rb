1ff6c8fa1a627c280955a38bc6d080cde349b59f
Netventief <info@netventief.nl>
2014-06-19 12:48:06 +0200

    Add validations to Sport model

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -6,4 +6,19 @@ class Sport < ActiveRecord::Base
   scope :by_user, -> (user_id) { where(user_id: user_id) }
   scope :by_date, -> (date) { where(date: date) }
 
+  # Validations
+  validates :date,
+            :populair_sport_id,
+            :duration,
+            :distance,
+            presence: true
+
+  # validates_numericality_of :duration,
+  #                           :distance,
+  #                           :populair_sport_id,
+  #                           greater_than: 0
+
+
+
+
 end
