4c67c65bb8d86206e5d63d417cd6bcb6541e3442
Netventief <info@netventief.nl>
2014-06-23 16:38:40 +0200

    Set oauth by sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,6 +49,8 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
       user.avatar_remote_url = auth.info.image
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
     end
   end
 
