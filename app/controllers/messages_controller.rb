ac7c42861c543d2d08c75595d18a957c3f3758c2
Jurgen Bosma <jurrieb@gmail.com>
2014-06-24 15:39:52 +0200

    added user id to users

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.last(5)
+    @messages = Message.where(user_id: current_user.id).order(:created_at).limit(5)
     respond_to do |format|
       format.html
       format.json { render :json => @messages}
