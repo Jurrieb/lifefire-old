750af18fba18f2a7b47ae2aa34e4a12fe3c5454c
Netventief <info@netventief.nl>
2014-06-24 11:43:17 +0200

    Remove some unnassesary code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,10 +2,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # Find of create user
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
-
     # Create relations
     @user.create_relations
-
     # If there is a user (saved in db), log in!
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
