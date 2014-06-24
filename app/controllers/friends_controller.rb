dfa8af38161b8e752732c9a8e994dcc10cda08aa
Netventief <info@netventief.nl>
2014-06-24 10:54:09 +0200

    Add friends_controller

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,16 @@
+class FriendsController < ApplicationController
+  def index
+
+    if token = current_user.oauth_token
+      @graph = Koala::Facebook::GraphAPI.new(token)
+      @friends = @graph.get_connections('me', 'friends')
+    end
+  end
+
+  def write_on_wall
+    if token = current_user.oauth_token
+      @graph = Koala::Facebook::GraphAPI.new(token)
+      @graph.put_object('me', 'feed', :message => "I am writing on my wall!")
+    end
+  end
+end
