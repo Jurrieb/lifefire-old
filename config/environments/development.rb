959a01650844002d167c70b346fab4067a7a336d
Netventief <info@netventief.nl>
2014-06-26 14:56:23 +0200

    Better sprocket error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
+  # Better sprockets error
+  config.assets.raise_production_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
