00879bd5d297f569b15218f17b42c85698093c8e
Netventief <info@netventief.nl>
2014-06-26 09:36:30 +0200

    Add faker to user Factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     password { SecureRandom.hex(10) }
     provider 'Facebook'
     uid 9999
-    name 'Test user'
+    name { Faker::Name.name }
     karmapoints 0
     oauth_token 'test'
     oauth_expires_at DateTime.now + 1.week
