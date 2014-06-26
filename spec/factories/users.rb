6374f30d08330c013abb2190f5f7038488be7029
Netventief <info@netventief.nl>
2014-06-26 09:19:04 +0200

    Add users factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define do
+  factory :user do
+    email 'test@test.nl'
+    password { SecureRandom.hex(10) }
+    provider 'Facebook'
+    uid 9999
+    name 'Test user'
+    karmapoints 0
+    oauth_token 'test'
+    oauth_expires_at DateTime.now + 1.week
+
+    # Needed relations
+    userDetail
+    userNotice
+    userPreference
+    userProfile
+    userSmokeAddiction
+  end
+end
