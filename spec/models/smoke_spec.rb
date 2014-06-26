69ab12553ba1e7fe7eef7d761c810898f609d7b2
Netventief <info@netventief.nl>
2014-06-26 12:49:04 +0200

    smoke spec

--- spec/models/smoke_spec.rb
+++ spec/models/smoke_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 describe Smoke do
+  # Subjects
+  let(:subject) { FactoryGirl.create(:smoke, user_id: 1) }
 
   # Relations
   it { should belong_to(:user) }
 
+  # Custom methods
+  describe '#add_to_smoking_counter' do
+    it ', value = 0' do
+      # Check increment (returns boolean)
+      expect(subject.add_to_smoking_counter(0)).to be_truthy
+    end
+
+    it ' ,value = random' do
+      # Check increment (returns boolean)
+      expect(subject.add_to_smoking_counter(rand(1..40))).to be_truthy
+    end
+  end
 end
