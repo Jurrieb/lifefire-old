9c697a527be2d07563467819f6c5ee0c71b22ccd
Netventief <info@netventief.nl>
2014-06-26 10:47:13 +0200

    Add realtions users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,10 +12,44 @@ describe User do
   it { should have_one(:userProfile) }
   it { should have_one(:userSmokeAddiction) }
 
+  it { should have_many(:smokes) }
+  it { should have_many(:sports) }
+  it { should have_many(:messages) }
 
   # Custom functions
-  it 'Gives name of user' do
-    expect(subject.name).to eq(subject.name)
-  end
+
+
+
+  # Concerns tests
+
 
 end
+
+
+
+# class User < ActiveRecord::Base
+
+#   # Concerns
+#   include Smoking
+#   include Sporting
+
+
+#   # Create needed relations in sign_in
+#   def create_relations
+#     UserPreference.create(user_id: self.id) unless self.userPreference.present?
+#     UserProfile.create(user_id: self.id) unless self.userProfile.present?
+#     UserDetail.create(user_id: self.id) unless self.userDetail.present?
+#     UserNotice.create(user_id: self.id) unless self.userNotice.present?
+#     UserSmokeAddiction.create(user_id: self.id) unless self.userSmokeAddiction.present?
+#   end
+
+#   # User is following quit-smoking program?
+#   def smokes?
+#     self.userPreference.smokes?
+#   end
+
+#   # User is following sports program
+#   def sports?
+#     self.userPreference.sports?
+#   end
+# end
