fa8fee8fed6029d0edbd574c122d6bc49e2e49e2
Netventief <info@netventief.nl>
2014-07-01 11:13:12 +0200

    Add method to perform messaging as background job

--- app/workers/messages.rb
+++ app/workers/messages.rb
@@ -2,8 +2,11 @@ class Messages
   include Sidekiq::Worker
 
   # Create a new message for user
-  def create_message(user, status, mention)
-
+  def perform(user, message)
+    puts "I LIKE THE WAY YOU WORKING"
+    Message.create!( user_id: user,
+                     message: message,
+                     seen: false,
+                     karma: 0 )
   end
-
 end
