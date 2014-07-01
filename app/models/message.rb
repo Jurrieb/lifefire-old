641aeeb362769eaaf04d4ccde122588d5487f68a
Netventief <info@netventief.nl>
2014-07-01 10:15:30 +0200

    Add validations to model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,16 @@
 class Message < ActiveRecord::Base
+  # Relation
   belongs_to :user
+
+  # Validations
+  validates :name,
+            :karma,
+            :seen,
+            :user_id,
+            presence: true
+
+  # Validate if karma and user_id are greater than 0
+  validates_numericality_of :karma
+
+
 end
