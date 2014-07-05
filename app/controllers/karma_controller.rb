a030797c0b5902f49760db864684d0c7703229ee
Netventief <info@netventief.nl>
2014-07-05 17:48:10 +0200

    Define steps for karma

--- app/controllers/karma_controller.rb
+++ app/controllers/karma_controller.rb
@@ -1,2 +1,24 @@
 class KarmaController < ApplicationController
+
+
+
+  # Should be rake taskable, so initilize with user id
+
+  # Multipliers for day / week / month / year
+
+  # Multipliers for kcal
+
+  # Multipliers for days stopped smoking
+
+  # Multipliers for MG tar reduced
+
+  # Multiplier for saved euros
+
+  # Multipliers for days
+
+  # Invited friends?
+
+  # Setup profile
+
+  #
 end
