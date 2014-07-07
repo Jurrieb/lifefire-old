2243b43bc5cdacd8d5439b3d353321cea7fd8793
Netventief <info@netventief.nl>
2014-07-07 18:17:55 +0200

    Change to render nothing

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
 
+  # Update all messages to been seen by user
   def update_seen
-    Message.update_all(:seen => true, :user_id => current_user.id)
-    render layout: false
+    Message.update_all(seen: true, user_id: current_user.id)
+    render nothing: true
   end
-
 end
