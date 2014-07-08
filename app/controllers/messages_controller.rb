69895670e5bc5aaa8adc0e8f82a9f7b8706b5b8f
Netventief <info@netventief.nl>
2014-07-08 18:03:45 +0200

    Rebase to Faye::controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,15 @@
-class MessagesController < ApplicationController
+# class MessagesController < ApplicationController
 
-  # Update all messages to been seen by user
-  def update_seen
-    Message.update_all(seen: true, user_id: current_user.id)
-    render nothing: true
+#   # Update all messages to been seen by user
+#   def update_seen
+#     Message.update_all(seen: true, user_id: current_user.id)
+#     render nothing: true
+#   end
+# end
+
+
+class MessagesController < FayeRails::Controller
+  observe User, :after_save do |message|
+    MessagesController.publish('/message', new_widget.attributes)
   end
 end
