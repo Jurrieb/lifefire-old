fa36232555f6beb2d77446f13155b7ef1ac26d0a
Netventief <info@netventief.nl>
2014-07-08 19:35:51 +0200

    Add Faye to middleware

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module Lifefire
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :nl
+
+    # Faye messaging
+    config.middleware.use FayeRails::Middleware, mount: '/faye', :timeout => 25
+    config.middleware.delete Rack::Lock
   end
 end
