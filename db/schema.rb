96bb2800dcb28e1aa61e5658736f58e7ec6eefd1
Netventief <info@netventief.nl>
2014-07-08 18:01:36 +0200

    Add userhash to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707075011) do
+ActiveRecord::Schema.define(version: 20140708153637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 20140707075011) do
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
     t.string   "slug"
+    t.string   "user_hash"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
