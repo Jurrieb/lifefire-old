c2cad0a915e9e60f2a1e08012e3594816478dd04
Netventief <info@netventief.nl>
2014-07-09 10:59:35 +0200

    Edited user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,12 +8,7 @@ FactoryGirl.define do
     karma 0
     oauth_token { rand(1...9999) }
     oauth_expires_at DateTime.now + 1.week
-    avatar_remote_url ["http://static4.businessinsider.com/image/51cded17ecad04b340000000-480/bridget-casey.jpg",
-                       "http://static4.businessinsider.com/image/5275a3faeab8ea6d23d16dcc-480/barack-obama.jpg",
-                       "http://cdn.lovell.co.uk/wp-content/uploads/2012/11/our-people1.jpg",
-                       "http://static1.businessinsider.com/image/2e7a6c7983ab514833a9fa00-480/gisellebundchen.jpg"].sample
-
-    user_hash { Digest::SHA1.hexdigest("#{Faker::Name.name}-#{Faker::Internet.email}") }
+    user_hash "jflkasjfdifasfd890f8dsahfjkan329fdisa9fdshakjrn42ql"
 
     # Needed relations
     userDetail
