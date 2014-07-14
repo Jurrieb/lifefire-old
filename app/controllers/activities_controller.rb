819b59dd67d7619b71318d58e9841c3159741549
Netventief <info@netventief.nl>
2014-07-14 16:01:21 +0200

    Find activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,15 @@
 class ActivitiesController < ApplicationController
-  def index
-  end
+
+  before_action :user_activities, only: :index
 
   def show
+    @activity = Activity.find(params[:id])
+    @comment  = @activity.comments.new
+  end
+
+  private
+
+  def user_activities
+    @user = User.find(params[:friend_id])
   end
 end
