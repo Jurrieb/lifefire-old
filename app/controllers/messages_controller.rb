189eba4f3fd24840454a614ca506192a91eaf8d7
Netventief <info@netventief.nl>
2014-07-15 11:14:00 +0200

    Fix N+1 query messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,22 +1,10 @@
+# Fetches all messages from a user and friend
 class MessagesController < ApplicationController
-
   # Get messages till a week ago
   def index
-    @activities = PublicActivity::Activity.where(owner_id: user_ids)
-                                          .where('created_at > ?', 1.week.ago)
+    @activities = PublicActivity::Activity.where(owner_id: self_and_friends)
+                                          .includes(:owner, :recipient)
+                                          .limit(10)
                                           .order('id desc')
-  end
-
-  private
-  # Creates array of (user)IDs that is used for selecting messages
-  def user_ids
-    # Create new array
-    ids = []
-    # Set current user ID in array
-    ids << current_user.id
-    # For all friends, add to array
-    current_user.friends.each { |friend| ids << friend.id }
-    # Return array
-    ids
   end
 end
