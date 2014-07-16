65997d24fe0d4f889de7d43d03b747d1bb8c72ce
Netventief <info@netventief.nl>
2014-07-16 19:38:26 +0200

    Add assets folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :pty, true
 #set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads public/assets}
 
 # Default value for default_env is {}
 set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
