42b923e60821020a218eef77f07a42e65b8d7453
Netventief <info@netventief.nl>
2014-07-21 10:14:05 +0200

    Faye Initializer

--- config/initializers/faye.rb
+++ config/initializers/faye.rb
@@ -1,2 +1,8 @@
 # Ensure Faye is running for processes outside Rails.
 #Faye.ensure_reactor_running!
+
+config.middleware.use FayeRails::Middleware, mount: '/faye',
+                                             :timeout => 25,
+                                             server: 'passenger',
+                                             engine: {type: Faye::Redis,
+                                                      host: 'localhost'}
