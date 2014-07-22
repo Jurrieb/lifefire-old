6c530fe4be5eeab8272d808af342737e0e252ece
Netventief <info@netventief.nl>
2014-07-22 11:58:10 +0200

    Rspec runs again

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 RSpec.describe ActivitiesController, :type => :controller do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      expect(response).to be_success
-    end
-  end
-
   describe "GET 'show'" do
     it "returns http success" do
       get 'show'
